f0f09ff38cd16802201b74d6fcc2db6b8c36b15c
Mayank-Gupta-25 <mkg31122005@gmail.com>
2025-12-31 18:02:16 +0530

    Added CLI

--- src/main.c
+++ src/main.c
@@ -1,21 +1,72 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <datastore.h>
+
+#define MAX_INPUT 256
 
 int main(){
 
+    char input[MAX_INPUT];
     store_init();
 
-    store_set("name", "Mayank");
-    store_set("lang", "C");
-    store_set("project", "KV Store");
+    printf("In-Memory KV Store (type EXIT to quit)\n");
 
-    printf("name = %s\n", store_get("name"));
-    printf("lang = %s\n", store_get("lang"));
+    while(1){
+        printf("> ");
+        if(!fgets(input,MAX_INPUT,stdin)){
+            break;
+        }
 
-    store_delete("lang");
+        input[strcspn(input,"\n")] = 0;
+        if(strlen(input) == 0) continue;
 
-    store_print();
+        char* command = strtok(input, " ");
+
+        if(strcmp(command,"SET") == 0){
+            char* key = strtok(NULL, " ");
+            char* value = strtok(NULL, "");
+
+            if(!key || !value){
+                printf("ERR usage : SET key value\n");
+                continue;
+            }
+
+            store_set(key, value);
+            printf("OK\n");
+
+        }
+        else if(strcmp(command,"GET")==0){
+            char* key = strtok(NULL, " ");
+
+            if(!key){
+                printf("ERR usage: GET key value\n");
+                continue;
+            }
+
+            const char* value = store_get(key);
+            if(value){
+                printf("%s\n",value);
+            }else{
+                printf("(nil)\n");
+            }
+        }
+        else if(strcmp(command,"DEL") == 0){
+            char* key = strtok(NULL, " ");
+            if(!key){
+                printf("ERR usage: DEL key value\n");
+                continue;
+            }
+            store_delete(key);
+            printf("OK\n");
+        }
+        else if(strcmp(command,"EXIT")==0){
+            break;
+        }
+        else{
+            printf("ERR unknown command\n");
+        }
+    }
     store_free();
-
     return 0;
 }
