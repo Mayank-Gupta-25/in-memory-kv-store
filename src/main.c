4ee6b9ce29898f99d416760e876ac195dd52b380
Mayank-Gupta-25 <mkg31122005@gmail.com>
2026-01-01 18:40:06 +0530

    Added TCP server support using POSIX sockets

--- src/main.c
+++ src/main.c
@@ -1,72 +1,120 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
 #include <datastore.h>
 
-#define MAX_INPUT 256
+#define PORT 6379
+#define BUFFER_SIZE 512
 
-int main(){
-
-    char input[MAX_INPUT];
-    store_init();
-
-    printf("In-Memory KV Store (type EXIT to quit)\n");
-
+void handle_client(int client_fd){
+    char buffer[BUFFER_SIZE];
+    
     while(1){
-        printf("> ");
-        if(!fgets(input,MAX_INPUT,stdin)){
+        memset(buffer,0,BUFFER_SIZE);
+        
+        int bytes = recv(client_fd, buffer, BUFFER_SIZE-1, 0);
+        if(bytes <= 0){
             break;
         }
 
-        input[strcspn(input,"\n")] = 0;
-        if(strlen(input) == 0) continue;
+        buffer[strcspn(buffer,"\n")]  = 0 ;
+        char* command = strtok(buffer, " ");
 
-        char* command = strtok(input, " ");
+        if(!command) continue;
 
         if(strcmp(command,"SET") == 0){
             char* key = strtok(NULL, " ");
             char* value = strtok(NULL, "");
-
+        
             if(!key || !value){
-                printf("ERR usage : SET key value\n");
+                send(client_fd, "ERR\n", 4, 0);
                 continue;
             }
-
+        
             store_set(key, value);
-            printf("OK\n");
-
+            send(client_fd, "OK\n", 3, 0);
+            
         }
         else if(strcmp(command,"GET")==0){
             char* key = strtok(NULL, " ");
-
+            
             if(!key){
-                printf("ERR usage: GET key value\n");
+                send(client_fd,"ERR\n", 4, 0);
                 continue;
             }
-
+            
             const char* value = store_get(key);
             if(value){
-                printf("%s\n",value);
+                send(client_fd, value , strlen(value), 0);
+                send(client_fd, "\n", 1, 0);
             }else{
-                printf("(nil)\n");
+                send(client_fd, "(nil)\n", 6, 0);
             }
         }
         else if(strcmp(command,"DEL") == 0){
             char* key = strtok(NULL, " ");
             if(!key){
-                printf("ERR usage: DEL key value\n");
+                send(client_fd,"ERR\n", 4, 0);
                 continue;
             }
             store_delete(key);
-            printf("OK\n");
+            send(client_fd, "OK\n", 3, 0);
         }
         else if(strcmp(command,"EXIT")==0){
             break;
         }
         else{
-            printf("ERR unknown command\n");
+            send(client_fd, "ERR unknown command\n", 20, 0);
         }
     }
+    close(client_fd);
+}
+
+int main(){
+
+    int server_fd, client_fd;
+    struct sockaddr_in address ;
+    int addrlen = sizeof(address);
+    
+    store_init();
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+
+
+    if (listen(server_fd, 5) < 0) {
+        perror("listen");
+        exit(1);
+    }
+
+    printf("KV Store listening on port %d...\n", PORT);
+
+    client_fd = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen);
+    if (client_fd < 0) {
+        perror("accept");
+        exit(1);
+    }
+
+    printf("Client connected\n");
+    handle_client(client_fd);
+
+    close(server_fd);
     store_free();
     return 0;
 }
